3c176282f0ab6ce3aef83c70a9485c63f2202397
Declan Bernardin <dmjbernardin@yahoo.com>
2019-12-05 17:22:11 -0600

    added some chenges to file structure

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,1 +1,52 @@
 import React from 'react'; 
+import { View, StyleSheet, Text, TextInput, Button } from 'react-native';
+
+import Card from '../components/Card'
+
+const StartGameScreen = props => {
+    return(
+        <View style={styles.screen}>
+            <Text style={styles.title}>The Game Screen</Text>
+            <Card style={styles.inputContainer}>
+                <Text>Select a Number</Text>
+                <TextInput />
+                <View style={styles.buttonContainer}>
+                    <View style={styles.button}>
+                        <Button title="Reset" onPress={() => { }} color="#c717fc" />
+                    </View>
+                    <View style={styles.button}>
+                        <Button title="confirm" onPress={() => { }} color="#f7287b"/>
+                    </View>
+                </View>
+            </Card>
+        </View>
+    );
+}; 
+
+const styles = StyleSheet.create({
+    screen:{
+        flex: 1,
+        padding: 10,
+        alignItems: 'center',
+    },
+    title: {
+        fontSize: 20,
+        marginVertical: 10,
+    },
+    inputContainer: {
+        width: 300,
+        maxWidth: '80%',
+        alignItems: 'center',
+    },
+    buttonContainer: {
+        flexDirection: 'row',
+        width: '100%', 
+        justifyContent: 'space-between',
+        paddingHorizontal: 15 
+    },
+    button: {
+        width: 100
+    }
+}); 
+
+export default StartGameScreen; 
