c49ba527ab770a27a2a3880d7f2cb5c056ecf705
Declan Bernardin <dmjbernardin@yahoo.com>
2019-12-11 19:07:48 -0600

    finished game and added game mover screen

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -21,14 +21,17 @@ const GameScreen = props => {
         generateRandomBetween(1, 100, props.userChoice)
         );
 
+        const [rounds, setRounds] = useState(0); 
         const currentLow = useRef(1);
         const currentHigh= useRef(100);
 
+        const { userChoice, onGameOver } = props;
+
         useEffect(() => {
-            if (currentGuess === props.userChoice){
-                
+            if (currentGuess === userChoice){
+                onGameOver(rounds);
             }
-        });
+        }, [currentGuess, userChoice, onGameOver]);
 
         const nextGuessHandler = direction => {
             if ((direction === 'lower' && currentGuess < props.userChoice) || 
@@ -43,6 +46,7 @@ const GameScreen = props => {
             }
            const nextNumber = generateRandomBetween(currentLow.current, currentHigh.current, currentGuess);
            setCurrentGuess(nextNumber);
+           setRounds(curRounds => curRounds + 1);
         };
 
     return(
