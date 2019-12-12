2408961bfd1216f2bc2d96c43b7c21856d2abcf4
Declan Bernardin <dmjbernardin@yahoo.com>
2019-12-11 18:45:21 -0600

    finished the game and allows the computer to guess the right number

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,14 +1,75 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useState, useRef, useEffect } from 'react';
+import { View, Text, StyleSheet, Button, Alert} from 'react-native';
+
+import NumberContainer from '../components/numberContainer';
+import Card from '../components/Card';
+
+const generateRandomBetween = (min, max, exclude) => {
+    min = Math.ceil(min);
+    max = Math .floor(max);
+    const rndNum = Math.floor(Math.random() * (max - min)) + min; 
+    if (rndNum === exclude) {
+        return generateRandomBetween(min, max, exclude); 
+    } else {
+        return rndNum; 
+    }
+};
 
 const GameScreen = props => {
+
+    const [currentGuess, setCurrentGuess] = useState(
+        generateRandomBetween(1, 100, props.userChoice)
+        );
+
+        const currentLow = useRef(1);
+        const currentHigh= useRef(100);
+
+        useEffect(() => {
+            if (currentGuess === props.userChoice){
+                
+            }
+        });
+
+        const nextGuessHandler = direction => {
+            if ((direction === 'lower' && currentGuess < props.userChoice) || 
+            (direction === 'greater' && currentGuess > props.userChoice)){
+                Alert.alert('Don\'t Lie', 'I Can Tell When Your Lying!', [{text: 'Okay', style: 'cancel'}])
+                return;
+            }
+            if (direction === 'lower') {
+                currentHigh.current = currentGuess; 
+            } else {
+                currentLow.current = currentGuess;
+            }
+           const nextNumber = generateRandomBetween(currentLow.current, currentHigh.current, currentGuess);
+           setCurrentGuess(nextNumber);
+        };
+
     return(
-        
+       <View style={styles.screen}>
+           <Text>Opponent's Guess</Text>
+           <NumberContainer>{currentGuess}</NumberContainer>
+            <Card style={styles.buttonContainer}>
+                <Button title="Lower" onPress={nextGuessHandler.bind(this, 'lower')}/>
+                <Button title="Greater" onPress={nextGuessHandler.bind(this, 'greater')}/>
+            </Card>
+       </View>
     );
 }; 
 
 const styles = StyleSheet.create({
-
+    screen: {
+        flex: 1,
+        padding: 10,
+        alignItems: 'center', 
+    },
+    buttonContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        marginTop: 20,
+        width: 300, 
+        maxWidth: '80%'
+    }
 });
 
 export default GameScreen;
--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -40,7 +40,7 @@ const StartGameScreen = props => {
         <Card style={styles.summaryContainer}>
             <Text>You Selected</Text>
             <NumberContainer>{selectedNumber}</NumberContainer>
-            <Button title="Start Game"/>
+            <Button title="Start Game" onPress={() => props.onStartGame(selectedNumber)}/>
         </Card>;
     }
 
