75aa6c3e94f5e12cd34b123f7ee989449fd3b74e
Declan Bernardin <dmjbernardin@yahoo.com>
2020-01-06 13:42:38 -0600

    added some font files

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -84,6 +84,7 @@ const styles = StyleSheet.create({
     title: {
         fontSize: 20,
         marginVertical: 10,
+        fontFamily: 'open-sans-bold'
     },
     inputContainer: {
         width: 300,
